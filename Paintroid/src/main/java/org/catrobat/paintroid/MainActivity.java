620f9202733557f6ba92ab9b8fd8026e0fa81b00
Clemens Stary <c.stary@student.tugraz.at>
2016-08-10 11:49:45 +0200

    Action Bar

--- Paintroid/src/main/java/org/catrobat/paintroid/MainActivity.java
+++ Paintroid/src/main/java/org/catrobat/paintroid/MainActivity.java
@@ -59,12 +59,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends  OptionsMenuActivity {
 
 	public static final String EXTRA_INSTANCE_FROM_CATROBAT = "EXTRA_INSTANCE_FROM_CATROBAT";
 	public static final String EXTRA_ACTION_BAR_HEIGHT = "EXTRA_ACTION_BAR_HEIGHT";
@@ -247,12 +249,13 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreateOptionsMenu(menu);
 		mMenu = menu;
 		PaintroidApplication.menu = mMenu;
-		MenuInflater inflater = getSupportMenuInflater();
+		MenuInflater inflater = getMenuInflater();
 		if (PaintroidApplication.openedFromCatroid) {
 			inflater.inflate(R.menu.main_menu_opened_from_catroid, menu);
 		} else {
 			inflater.inflate(R.menu.main_menu, menu);
 		}
+
 
 		return true;
 	}
@@ -261,34 +264,34 @@ public class MainActivity extends AppCompatActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 
 		switch (item.getItemId()) {
-		case R.id.menu_item_back_to_catroid:
-			showSecurityQuestionBeforeExit();
-			return true;
-		case R.id.menu_item_terms_of_use_and_service:
-			DialogTermsOfUseAndService termsOfUseAndService = new DialogTermsOfUseAndService();
-			termsOfUseAndService.show(getSupportFragmentManager(),
-					"termsofuseandservicedialogfragment");
-			return true;
-		case R.id.menu_item_about:
-			DialogAbout about = new DialogAbout();
-			about.show(getSupportFragmentManager(), "aboutdialogfragment");
-			return true;
-		case R.id.menu_item_hide_menu:
-			setFullScreen(mToolbarIsVisible);
-			return true;
-		case android.R.id.home:
-			if (PaintroidApplication.openedFromCatroid) {
+			case R.id.menu_item_back_to_catroid:
 				showSecurityQuestionBeforeExit();
-			}
-			return true;
-			/* EXCLUDE PREFERENCES FOR RELEASE */
+				return true;
+			case R.id.menu_item_terms_of_use_and_service:
+				DialogTermsOfUseAndService termsOfUseAndService = new DialogTermsOfUseAndService();
+				termsOfUseAndService.show(getSupportFragmentManager(),
+						"termsofuseandservicedialogfragment");
+				return true;
+			case R.id.menu_item_about:
+				DialogAbout about = new DialogAbout();
+				about.show(getSupportFragmentManager(), "aboutdialogfragment");
+				return true;
+			case R.id.menu_item_hide_menu:
+				setFullScreen(mToolbarIsVisible);
+				return true;
+			case android.R.id.home:
+				if (PaintroidApplication.openedFromCatroid) {
+					showSecurityQuestionBeforeExit();
+				}
+				return true;
+			//* EXCLUDE PREFERENCES FOR RELEASE *//*
 			// case R.id.menu_item_preferences:
 			// Intent intent = new Intent(this, SettingsActivity.class);
 			// intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
 			// startActivity(intent);
 			// return false;
-		default:
-			return super.onOptionsItemSelected(item);
+			default:
+				return super.onOptionsItemSelected(item);
 		}
 	}
 
@@ -312,6 +315,7 @@ public class MainActivity extends AppCompatActivity {
 			switchTool(ToolType.BRUSH);
 		}
 	}
+
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
